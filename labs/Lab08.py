b4630b0b18aced563962870b0cb76502ff62e8dd
uc-ryan2c2 <ryan2c2@mail.uc.edu>
2021-10-31 18:54:28 -0400

    lab08

--- labs/Lab08.py
+++ labs/Lab08.py
@@ -1,6 +1,7 @@
 import requests, re
 from bs4 import BeautifulSoup
 
+#this will scrape a website and output a list of the current major league soccer teams
 data = requests.get("https://www.rookieroad.com/soccer/mls-teams-list/").content
 soup = BeautifulSoup(data, 'html.parser')
 #details = soup.find(href=re.compile('#'))
